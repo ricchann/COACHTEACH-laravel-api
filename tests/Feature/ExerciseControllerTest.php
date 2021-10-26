ac76a919043dd150b9f0a9e6505308a95552037c
riyo <second3to7@gmail.com>
2021-10-26 17:14:51 +0900

    Laravel API テスト問題

---
+++ tests/Feature/ExerciseControllerTest.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+use App\Models\Exercise;
+
+class ExerciseControllerTest extends TestCase
+{
+    use RefreshDatabase;
+    /**
+     * A basic feature test example.
+     *
+     * @return void
+     */
+    public function test_index_exercise()
+    {
+        $item = Exercise::factory()->create();
+        $response = $this->get('/api/v1/exercise');
+        $response->assertStatus(200);
+        $response->assertJsonFragment([
+            'name' => $item->name,
+            'email' => $item->email,
+            'profile' => $item->profile,
+        ]);
+    }
+    public function test_store_exercise()
+    {
+        $data = [
+            'name' => 'exercise',
+            'email' => 'exercise@example.com',
+            'profile' => 'exercise_profiles',
+        ];
+        $response = $this->post('/api/v1/exercise', $data);
+        $response->assertStatus(201);
+        $response->assertJsonFragment($data);
+        $this->assertDatabaseHas('exercises', $data);
+    }
+    public function test_show_exercise()
+    {
+        $item = Exercise::factory()->create();
+        $response = $this->get('/api/v1/exercise/' . $item->id);
+        $response->assertStatus(200);
+        $response->assertJsonFragment([
+            'name' => $item->name,
+            'email' => $item->email,
+            'profile' => $item->profile,
+        ]);
+
+        $responseErr = $this->get('/api/v1/exercise/' . $item->id + 1);
+        $responseErr->assertStatus(404);
+    }
+    public function test_update_exercise()
+    {
+        $item = Exercise::factory()->create();
+        $data = [
+            'name' => 'exercise_update',
+            'email' => 'exercise_update@example.com',
+            'profile' => 'exercise_update_profile',
+        ];
+        $response = $this->put('/api/v1/exercise/' . $item->id, $data);
+        $response->assertStatus(200);
+        $this->assertDatabaseHas('exercises', $data);
+
+        $responseErr = $this->put('/api/v1/exercise/' . $item->id + 1, $data);
+        $responseErr->assertStatus(404);
+    }
+    public function test_destroy_exercise()
+    {
+        $item = Exercise::factory()->create();
+        $response = $this->delete('/api/v1/exercise/' . $item->id);
+        $response->assertStatus(200);
+        $this->assertDeleted($item);
+
+        $responseErr = $this->delete('/api/v1/exercise/' . $item->id + 1);
+        $responseErr->assertStatus(404);
+    }
+}
